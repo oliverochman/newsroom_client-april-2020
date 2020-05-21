41efcf1cf5e85dce445570d0ddbd3a55f97c4fb3
pauline_barnades <barnades.pauline@gmail.com>
2020-05-21 10:27:27 +0200

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ArticleList from "./components/ArticleList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <ArticleList></ArticleList>
     </div>
   );
