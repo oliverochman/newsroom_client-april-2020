f1707791ffb0d0a6154210b0d8f95e6e90463427
japh <jenny.scherr@svenskaspel.se>
2020-05-21 22:09:01 +0200

    refactured the code with useState

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,39 +1,45 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Placeholder, Grid } from "semantic-ui-react";
 
-export default class ArticleList extends Component {
-  state = {
-    articleList: [],
-  };
-  async componentDidMount() {
-    try {
-      const response = await axios.get("/articles");
-      this.setState({ articleList: response.data.articles });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+const ArticleList = () => {
+  const [articleList, setArticleList] = useState([]);
+  const [message] = useState();
 
-  render() {
-    let articleslist = this.state.articleList.map((article) => {
-      return (
-        <Grid.Row width={3} centered>
-          <Placeholder
-            style={{ height: 250, width: 400 }}
-            key={article.id}
-            id={"article-" + article.id}
-          >
-            <Placeholder.Image />
-            <h5 style={{ textAlign: "center" }}>{article.title}</h5>
-          </Placeholder>
-        </Grid.Row>
-      );
-    });
+  useEffect(() => {
+    const fetchArticleList = async () => {
+      try {
+        const response = await axios.get("/articles");
+        setArticleList(response.data.articles);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchArticleList();
+  }, []);
+
+  let articleslist = articleList.map((article) => {
     return (
+      <Grid.Row width={3} centered>
+        <Placeholder
+          style={{ height: 250, width: 400 }}
+          key={article.id}
+          id={"article-" + article.id}
+        >
+          <Placeholder.Image />
+          <h5 style={{ textAlign: "center" }}>{article.title}</h5>
+        </Placeholder>
+      </Grid.Row>
+    );
+  });
+
+  return (
+    <div>
       <Grid columns={3} divided centered>
         {articleslist}
       </Grid>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default ArticleList;
