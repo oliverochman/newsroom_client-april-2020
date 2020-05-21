74119a9d80c01521449fa9687d856a346c8a425d
japh <jenny.scherr@svenskaspel.se>
2020-05-21 10:52:52 +0200

    fixture added for article list, feature test added to view list

---
+++ cypress/integration/visitorCanViewAllListedArticles.feature.js
@@ -0,0 +1,18 @@
+describe("visitor can view all listed articles", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/articles",
+      response: "fixture:articleList.json",
+    });
+    cy.visit("/");
+  });
+  it("header is shown", () => {
+    cy.get("#header");
+  });
+  it("articles is shown", () => {
+    cy.get("#title-article-1").should("contain", "title1");
+    cy.get("#title-article-2").should("contain", "title2");
+  });
+});
