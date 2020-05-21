995572e18acf0543ba858c52edf59f9a67b11a59
pauline_barnades <barnades.pauline@gmail.com>
2020-05-21 11:19:21 +0200

    Add axios and create get request

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import axios from "axios"
 
-const ArticleList = () => {
+const ArticleList = async () => {
+  response = await axios.get('/articles');
   return (
     <div>
-      <h4>First article</h4>
+      <h4>
+
+      </h4>
     </div>
   )
 }
