5cabb8c48f2c52e53e635127404486a2f73eb535
oliverochman <oliver.ochman@hotmail.com>
2020-05-22 18:00:58 +0200

    configure routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import { 
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 import ArticleList from "./components/ArticleList";
 import Header from "./components/Header";
+import SingleArticle from "./components/SingleArticle"
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Switch>
-            <Route exact path = "/" component = {ArticleList}></Route>
-            <Route exact path = "/article" component = {SpecificArticle}></Route>
-        </Switch> */}
-      <ArticleList />
+      <Switch>
+        <Route exact path = "/" component={ArticleList}></Route>
+        <Route exact path = "/article/:id" component={SingleArticle}></Route>
+      </Switch>
     </>
   );
 }
