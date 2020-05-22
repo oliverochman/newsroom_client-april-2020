cf5c2d16e1fe189df5355c91c63a7d790f4830a3
pauline_barnades <barnades.pauline@gmail.com>
2020-05-22 18:33:06 +0200

    fixing the router, changing component name to SingleArticle, editing the test

--- cypress/integration/visitorCanReadSpecificArticle.feature.js
+++ cypress/integration/visitorCanReadSpecificArticle.feature.js
@@ -1,4 +1,4 @@
-describe('visitor can read a specific article', () => {
+describe("visitor can read a specific article", () => {
   before(() => {
     cy.server();
     cy.route({
@@ -6,25 +6,20 @@ describe('visitor can read a specific article', () => {
       url: "http://localhost:3000/api/articles",
       response: "fixture:article_list.json",
     });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/articles/1",
+      response: "fixture:single_article.json",
+    });
     cy.visit("/");
-  });
-
-  it("article list is displayed", () => {
-    cy.get("#article-1").should("contain", "title 1").click();
-  });
-
-  it( 'article is displayed', () => {
-    before(() => {
-      cy.route({
-        method: "GET",
-        url: "http://localhost:3000/api/articles/1",
-        response: "fixture:article_list.json",
-      });
-      cy.visit("/");
+    cy.get("#article-1").within(() => {
+      cy.get("#article-title").click();
     })
-    // cy.get("#article-1-title").should("contain", "Title 1")
-    // cy.get("#article-1-body").should("contain", "Lorem ipsum")
   });
-  
-  
-})
+
+  it("article is displayed", () => {
+    cy.get("#article-1-title").should("contain", "Title 1");
+    cy.get("#article-1-body").should("contain", "Lorem ipsum");
+    cy.get("#article-1").should("not.exist")
+  });
+});
