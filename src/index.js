ca9f681c0e72b5fc1cff83f67338a1fd73dd42b9
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-23 11:56:32 +0200

    styling SingleArticle view updating fixture to include longer body modifying layout of view importing the fonts stylesheet in index.js instead of header

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
+import './css/Header.css'
+
 
 
 axios.defaults.baseURL = "http://localhost:3000/api";
