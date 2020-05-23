4856a0c12677e1c4bbaa3cc13671fa978d635f27
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-23 11:56:32 +0200

    styling SingleArticle view updating fixture to include longer body modifying layout of view importing the fonts stylesheet in index.js instead of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
-import "../css/Header.css"
 
 const Header = () => {
   return (
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Placeholder, Grid } from "semantic-ui-react";
+import { Placeholder, Grid, Container } from "semantic-ui-react";
 import axios from 'axios';
 
 const SingleArticle = (props) => {
@@ -14,23 +14,25 @@ const SingleArticle = (props) => {
   }, [])
 
   return (
-    <>
-      <Grid.Row width={3} centered>
-        <Placeholder
-          style={{ height: 250, width: 400 }}
-          key={article.id}
-          id={"article-" + article.id + "-title"}
-        >
-          <Placeholder.Image />
-          <h5 style={{ textAlign: "center" }}>{article.title}</h5>
-        </Placeholder>
-      </Grid.Row>
-      <Grid.Row width={3} centered>
-        <Placeholder key={article.id} id={"article-" + article.id + "-body"}>
-          <p>{article.body}</p>
-        </Placeholder>
-      </Grid.Row>
-    </>
+    <Container align="center" style={{paddingTop: "45px", width: "55%"}}>
+      <Grid stretched>
+        <Grid.Row centered>
+          <Placeholder
+            style={{ height: 250, width: 900 }}
+            key={article.id}
+            id={"article-" + article.id + "-title"}
+          >
+            <Placeholder.Image />
+            <h5 style={{ textAlign: "center" }}>{article.title}</h5>
+          </Placeholder>
+        </Grid.Row>
+        <Grid.Row centered>
+            <p key={article.id} id={"article-" + article.id + "-body"} style={{ textAlign: "left" }}>
+              {article.body}
+            </p>
+        </Grid.Row>
+      </Grid>
+    </Container>
   );
 };
 export default SingleArticle;
