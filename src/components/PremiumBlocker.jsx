8327465ac3a5be13bc99230c3ace142983da3fd2
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-26 14:36:07 +0200

    fix formatting in file

--- src/components/PremiumBlocker.jsx
+++ src/components/PremiumBlocker.jsx
@@ -5,12 +5,12 @@ import '../css/PremiumBlocker.css'
 const PremiumBlocker = () => {
   return (
     <>
-    <div id="cover"></div>
-    <div id="premium-blocker">
-      <h4>This is a premium article</h4>
-      <h4><Icon name="lock" /></h4>
-      <h4>Sign in to view this article</h4>
-    </div>
+      <div id="cover"></div>
+      <div id="premium-blocker">
+        <h4>This is a premium article</h4>
+        <h4><Icon name="lock" /></h4>
+        <h4>Sign in to view this article</h4>
+      </div>
     </>
   )
 }
