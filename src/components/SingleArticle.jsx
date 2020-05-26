033e63e1ba85f91b73f0d1522c5f32f955136bbf
japh <jenny.scherr@svenskaspel.se>
2020-05-26 08:56:59 +0200

    updated the naming of the key to match response from api

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -34,7 +34,7 @@ const SingleArticle = (props) => {
             id={"article-" + article.id + "-date"}
             style={{ textAlign: "left" }}
           >
-            Publish date: {article.publish_date}
+            Publish at: {article.published_at}
           </p>
         </Grid.Row>
         <Grid.Row centered>
