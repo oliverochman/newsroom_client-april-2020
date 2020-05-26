226357c5d4224875ceaa0faa917735901bd82f1d
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-26 14:19:49 +0200

    adding styling with fade over content

--- src/components/PremiumBlocker.jsx
+++ src/components/PremiumBlocker.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Icon } from 'semantic-ui-react'
+import '../css/PremiumBlocker.css'
 
 const PremiumBlocker = () => {
   return (
+    <>
+    <div id="cover"></div>
     <div id="premium-blocker">
-      <h4>This is a premium article</h4><br />
+      <h4>This is a premium article</h4>
       <h4><Icon name="lock" /></h4>
       <h4>Sign in to view this article</h4>
     </div>
+    </>
   )
 }
 
