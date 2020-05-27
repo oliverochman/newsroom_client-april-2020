c771831b84d459a85daee63ae2c1bbf625dc68fe
pauline_barnades <barnades.pauline@gmail.com>
2020-05-27 21:25:34 +0200

    Adding button with onclick function

--- src/components/CreateSubscription.jsx
+++ src/components/CreateSubscription.jsx
@@ -8,12 +8,16 @@ import {
 
 
 const CreateSubscription = () => {
+  const submitPayment = () => {} 
   return (
     <div id='payment-interface'>
-      <CardNumberElement />
-      <CardExpiryElement />
-      <CardCVCElement />
+      <CardNumberElement id='cardnumber' />
+      <CardExpiryElement id='exp-date' />
+      <CardCVCElement id='cvc' />
+      <button onClick={submitPayment}>Submit</button>
+
     </div>
+
   )
 }
 
