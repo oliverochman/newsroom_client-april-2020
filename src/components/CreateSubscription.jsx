d8f6cd8e99ec921baf31f9be2537ce5dfe5eac53
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-27 21:48:37 +0200

    adding functionality to the onClick function, displaying success message

--- src/components/CreateSubscription.jsx
+++ src/components/CreateSubscription.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React , { useState } from "react";
 import {
   injectStripe,
   CardNumberElement,
   CardExpiryElement,
-  CardCVCElement
-} from 'react-stripe-elements'
+  CardCVCElement,
+} from "react-stripe-elements";
+import Axios from "axios";
 
+const CreateSubscription = props => {
+  //const headers = JSON.parse(localStorage.getItem('J-tockAuth-Storage'))
+  const [subscriberStatus, setSubscriberStatus] = useState(false)
+  const [subscriptionMessage, setSubscriptionMessage] = useState("")
+  const submitPayment = async () => {
+    const stripeResponse = await props.stripe.createToken()
 
-const CreateSubscription = () => {
-  const submitPayment = () => {} 
+    try {
+      const paymentStatus = await Axios.post('/subscriptions',
+        { stripeToken: stripeResponse.token.id },
+        //{ headers: headers }
+      )
+      if (paymentStatus.status === 200) {
+        setSubscriberStatus(true)
+        setSubscriptionMessage(paymentStatus.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
   return (
-    <div id='payment-interface'>
-      <CardNumberElement id='cardnumber' />
-      <CardExpiryElement id='exp-date' />
-      <CardCVCElement id='cvc' />
-      <button onClick={submitPayment}>Submit</button>
+    <>
+    {subscriberStatus ?
+      <span id="subscription-message">
+      {subscriptionMessage}</span>
+      :
+      <div id="payment-interface">
+        <CardNumberElement id="cardnumber" />
+        <CardExpiryElement id="exp-date" />
+        <CardCVCElement id="cvc" />
+        <button onClick={submitPayment}>Submit</button>
+      </div>
+    }
+  </>
+  );
+};
 
-    </div>
-
-  )
-}
-
-export default injectStripe(CreateSubscription)
+export default injectStripe(CreateSubscription);
