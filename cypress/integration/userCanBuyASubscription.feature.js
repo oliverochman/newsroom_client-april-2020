93f040d6bbbd37f50dfea706f2dfce30e44bd0cc
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-29 14:52:26 +0200

    displaying client-side error messages

--- cypress/integration/userCanBuyASubscription.feature.js
+++ cypress/integration/userCanBuyASubscription.feature.js
@@ -1,7 +1,7 @@
-describe('User can purchase a subscription on the subscribe page', () => {
+describe("User can purchase a subscription on the subscribe page", () => {
   beforeEach(() => {
-		cy.server()
-		
+    cy.server();
+
     cy.route({
       method: "GET",
       url: "http://localhost:3000/api/articles",
@@ -12,66 +12,74 @@ describe('User can purchase a subscription on the subscribe page', () => {
       url: "http://localhost:3000/api/auth/*",
       response: "fixture:successful_login.json",
       headers: {
-        uid:"user@mail.com"
-      }
-    })
+        uid: "user@mail.com",
+      },
+    });
     cy.route({
       method: "GET",
       url: "http://localhost:3000/api/auth/*",
       response: "fixture:successful_login.json",
       headers: {
-        uid:"user@mail.com"
-      }
-    })
-    cy.visit("/")
-    cy.get('a > #login').click()
-    cy.get('#login-form').within(() => {
-      cy.get('#email').type('user@mail.com');
-      cy.get('#password').type('password');
-      cy.get('Button').contains('Submit').click();
+        uid: "user@mail.com",
+      },
     });
-    cy.get('#subscription-link').contains('Subscribe').click()
-  })
-  
-  
-  describe('Successfully', () => {
-    beforeEach(() => {
-      cy.route({
-        method: "POST",
-        url: "**/subscriptions",
-        response: { message: "Transaction was successful" }
-      })
-    })
-    it('By filling up subscription and payment form', () => {
-    cy.get('#payment-interface').should('be.visible')
-    cy.wait(1000)
-    cy.typeInStripeElement("cardnumber", "4242424242424242")
-    cy.typeInStripeElement("exp-date", "12/21")
-    cy.typeInStripeElement("cvc", "123")
-    cy.get('button').contains('Confirm Payment').click()
-    cy.get('#transaction-message').should('contain','Transaction was successful')
-    cy.wait(2000)
-    cy.get('#transaction-message').should('not.be.visible')
-    cy.get('#subscriber-message').should('contain','You are a subscriber!')
-    })
-  })
+    cy.visit("/");
+    cy.get("a > #login").click();
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("user@mail.com");
+      cy.get("#password").type("password");
+      cy.get("Button").contains("Submit").click();
+    });
+    cy.get("#subscription-link").contains("Subscribe").click();
+  });
 
-  describe('Unsuccessfully', () => {
+  describe("Successfully", () => {
     beforeEach(() => {
       cy.route({
         method: "POST",
         url: "**/subscriptions",
-        response: { message: "An error has occured!" }, status: 400
-      })
+        response: { message: "Transaction was successful" },
+      });
+    });
+    it("By filling up subscription and payment form", () => {
+      cy.get("#payment-interface").should("be.visible");
+      cy.wait(1000);
+      cy.typeInStripeElement("cardnumber", "4242424242424242");
+      cy.typeInStripeElement("exp-date", "12/21");
+      cy.typeInStripeElement("cvc", "123");
+      cy.get("button").contains("Confirm Payment").click();
+      cy.get("#transaction-message").should(
+        "contain",
+        "Transaction was successful"
+      );
+      cy.wait(2000);
+      cy.get("#transaction-message").should("not.be.visible");
+      cy.get("#subscriber-message").should("contain", "You are a subscriber!");
+    });
+  });
+
+  describe("Unsuccessfully", () => {
+    it("Displays error messages arriving from back-end", () => {
+      cy.route({
+        method: "POST",
+        url: "**/subscriptions",
+        response: { message: "An error has occured!" },
+        status: 400,
+      });
+      cy.wait(1000);
+      cy.typeInStripeElement("cardnumber", "4242424242424242");
+      cy.typeInStripeElement("exp-date", "12/21");
+      cy.typeInStripeElement("cvc", "123");
+      cy.get("button").contains("Confirm Payment").click();
+      cy.get("#error-message").should("contain", "An error has occured!");
+    });
+
+    it('Displays error messages regarding filling out fields', () => {
+      cy.wait(1000);
+      cy.typeInStripeElement("cardnumber", "4242424242424242");
+      cy.typeInStripeElement("exp-date", "12/10");
+      cy.get("button").contains("Confirm Payment").click();
+      cy.get("#error-message").should("contain", "Your card's expiration year is in the past.");
     })
-    it('Display error message', () => {
-  cy.get('#payment-interface').should('be.visible')
-    cy.wait(1000)
-    cy.typeInStripeElement("cardnumber", "4242424242424242")
-    cy.typeInStripeElement("exp-date", "12/21")
-    cy.typeInStripeElement("cvc", "123")
-    cy.get('button').contains('Confirm Payment').click()
-    cy.get('#error-message').should('contain','An error has occured!')
-  })
-  })
-}) 
+  });
+});
--- src/components/CreateSubscription.jsx
+++ src/components/CreateSubscription.jsx
@@ -10,33 +10,34 @@ import "../css/CreateSubscription.css";
 import { Segment, Button, Grid, Input } from "semantic-ui-react";
 
 const CreateSubscription = (props) => {
-  const headers = JSON.parse(localStorage.getItem('J-tockAuth-Storage'))
+  const headers = JSON.parse(localStorage.getItem("J-tockAuth-Storage"));
   const [subscriberStatus, setSubscriberStatus] = useState(false);
   const [transactionMessage, setTransactionMessage] = useState("");
-  
 
   const submitPayment = async () => {
     const stripeResponse = await props.stripe.createToken();
 
-    try {
-      const paymentStatus = await Axios.post(
-        "/subscriptions",
-        { stripeToken: stripeResponse.token.id },
-        { headers: headers }
-      );
-      if (paymentStatus.status === 200) {
-        setSubscriberStatus(true);
-        setTransactionMessage(paymentStatus.data.message);
-        
-        setTimeout(() => {
-          setTransactionMessage("");
-        }, 4000);
+    if (stripeResponse.error) {
+      setTransactionMessage(stripeResponse.error.message);
+    } else {
+      try {
+        const paymentStatus = await Axios.post(
+          "/subscriptions",
+          { stripeToken: stripeResponse.token.id },
+          { headers: headers }
+        );
+        if (paymentStatus.status === 200) {
+          setSubscriberStatus(true);
+          setTransactionMessage(paymentStatus.data.message);
+
+          setTimeout(() => {
+            setTransactionMessage("");
+          }, 4000);
+        }
+      } catch (error) {
+        setTransactionMessage(error.response.data.message);
+        console.log(error);
       }
-    
-    } catch (error) { 
-      
-      setTransactionMessage(error.response.data.message)
-         console.log(error);
     }
   };
 
@@ -48,48 +49,48 @@ const CreateSubscription = (props) => {
           <h1 id="subscriber-message">You are a subscriber!</h1>
         </div>
       ) : (
-          <>
-          <h4 id='error-message'>{transactionMessage}</h4>
-          <div className='payment-container'>
-        <Segment padded inverted id="payment-interface">
-          <h3>Become a subscriber today!</h3>
-          <h4>1 month for only $10!</h4>
-          <Grid>
-            <p><em>subscription ends automatically after 30 days</em></p>
-            <Grid.Row>
-              <Grid.Column width={16}>
-                <label htmlFor="cardnumber">Card number</label>
-                <Segment>
-                  <CardNumberElement id="cardnumber" />
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={9}>
-                <label htmlFor="exp-date">Expiry date</label>
-                <Segment>
-                  <CardExpiryElement id="exp-date" />
-                </Segment>
-              </Grid.Column>
-              <Grid.Column width={7}>
-                <label htmlFor="cvc">CVC</label>
-                <Segment>
-                  <CardCVCElement id="cvc" />
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-          <div className="button-div">
-            <Button basic inverted onClick={submitPayment}>
-                Confirm Payment
-            </Button>
-              
-          </div>
+        <>
+          <h4 id="error-message">{transactionMessage}</h4>
+          <div className="payment-container">
+            <Segment padded inverted id="payment-interface">
+              <h3>Become a subscriber today!</h3>
+              <h4>1 month for only $10!</h4>
+              <Grid>
+                <p>
+                  <em>subscription ends automatically after 30 days</em>
+                </p>
+                <Grid.Row>
+                  <Grid.Column width={16}>
+                    <label htmlFor="cardnumber">Card number</label>
+                    <Segment>
+                      <CardNumberElement id="cardnumber" />
+                    </Segment>
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column width={9}>
+                    <label htmlFor="exp-date">Expiry date</label>
+                    <Segment>
+                      <CardExpiryElement id="exp-date" />
+                    </Segment>
+                  </Grid.Column>
+                  <Grid.Column width={7}>
+                    <label htmlFor="cvc">CVC</label>
+                    <Segment>
+                      <CardCVCElement id="cvc" />
+                    </Segment>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+              <div className="button-div">
+                <Button basic inverted onClick={submitPayment}>
+                  Confirm Payment
+                </Button>
+              </div>
             </Segment>
-            </div>
-            </>
+          </div>
+        </>
       )}
-
     </div>
   );
 };
