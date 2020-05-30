09074d652cef804749d51f5fe1ff41595806e4fe
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-30 14:50:52 +0200

    extending test with test for when not logged in

--- cypress/integration/userCanBuyASubscription.feature.js
+++ cypress/integration/userCanBuyASubscription.feature.js
@@ -1,47 +1,26 @@
 describe("User can purchase a subscription on the subscribe page", () => {
   beforeEach(() => {
     cy.server();
-
     cy.route({
       method: "GET",
       url: "http://localhost:3000/api/articles",
       response: "fixture:article_list.json",
     });
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/auth/*",
-      response: "fixture:successful_login.json",
-      headers: {
-        uid: "user@mail.com",
-      },
-    });
-    cy.route({
-      method: "GET",
-      url: "http://localhost:3000/api/auth/*",
-      response: "fixture:successful_login.json",
-      headers: {
-        uid: "user@mail.com",
-      },
-    });
     cy.visit("/");
-    cy.get("a > #login").click();
-    cy.get("#login-form").within(() => {
-      cy.get("#email").type("user@mail.com");
-      cy.get("#password").type("password");
-      cy.get("Button").contains("Submit").click();
-    });
-    cy.get("#subscription-link").contains("Subscribe").click();
   });
 
-  describe("Successfully", () => {
+  describe("Successfully when logged in", () => {
     beforeEach(() => {
       cy.route({
         method: "POST",
         url: "**/subscriptions",
         response: { message: "Transaction was successful" },
       });
+      cy.logIn();
+      cy.get("#subscription-link").contains("Subscribe").click();
     });
-    it("By filling up subscription and payment form", () => {
+
+    it("by filling out payment form", () => {
       cy.get("#payment-interface").should("be.visible");
       cy.wait(1000);
       cy.typeInStripeElement("cardnumber", "4242424242424242");
@@ -59,27 +38,62 @@ describe("User can purchase a subscription on the subscribe page", () => {
   });
 
   describe("Unsuccessfully", () => {
-    it("Displays error messages arriving from back-end", () => {
+    it("when the transaction was unsuccessful", () => {
       cy.route({
         method: "POST",
         url: "**/subscriptions",
-        response: { message: "An error has occured!" },
+        response: { message: "There was a problem with your transaction" },
         status: 400,
       });
+      cy.logIn();
+      cy.get("#subscription-link").contains("Subscribe").click();
       cy.wait(1000);
       cy.typeInStripeElement("cardnumber", "4242424242424242");
       cy.typeInStripeElement("exp-date", "12/21");
       cy.typeInStripeElement("cvc", "123");
       cy.get("button").contains("Confirm Payment").click();
-      cy.get("#error-message").should("contain", "An error has occured!");
+      cy.get("#error-message").should(
+        "contain",
+        "There was a problem with your transaction"
+      );
     });
 
-    it('Displays error messages regarding filling out fields', () => {
+    it("by entering incomplete data", () => {
+      cy.logIn();
+      cy.get("#subscription-link").contains("Subscribe").click();
       cy.wait(1000);
       cy.typeInStripeElement("cardnumber", "4242424242424242");
       cy.typeInStripeElement("exp-date", "12/10");
       cy.get("button").contains("Confirm Payment").click();
-      cy.get("#error-message").should("contain", "Your card's expiration year is in the past.");
-    })
+      cy.get("#error-message").should(
+        "contain",
+        "Your card's expiration year is in the past."
+      );
+    });
+  });
+
+  describe("Visitors and not logged-in users", () => {
+    beforeEach(() => {
+      cy.get("#subscription-link").contains("Subscribe").click();
+    });
+
+    it("cannot see the payment form", () => {
+      cy.get("#payment-interface").should("not.exist");
+    });
+
+    it("see a message", () => {
+      cy.get("#subscribe-today").should(
+        "contain",
+        "Become a subscriber today!"
+      );
+    });
+
+    it("see a link to sign up/log in page", () => {
+      cy.get("#sign-in-sign-up")
+        .should("contain", "Log in or sign up to proceed")
+        .click();
+      cy.get("#login-form")
+        .should("be.visible")
+    });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,3 +6,28 @@ Cypress.Commands.add('typeInStripeElement', (element, value) => {
 			.type(value, {delay: 10})
 	})
 })
+
+Cypress.Commands.add('logIn', () => {
+  cy.route({
+    method: "POST",
+    url: "http://localhost:3000/api/auth/*",
+    response: "fixture:successful_login.json",
+    headers: {
+      uid: "user@mail.com",
+    },
+  });
+  cy.route({
+    method: "GET",
+    url: "http://localhost:3000/api/auth/*",
+    response: "fixture:successful_login.json",
+    headers: {
+      uid: "user@mail.com",
+    },
+  });
+  cy.get("a > #login").click();
+  cy.get("#login-form").within(() => {
+    cy.get("#email").type("user@mail.com");
+    cy.get("#password").type("password");
+    cy.get("Button").contains("Submit").click();
+  });
+})
